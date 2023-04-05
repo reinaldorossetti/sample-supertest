a24ac517edd1685883bdaeb4cb95c7f873092b35
Reinaldo <reinaldo.rossetti@outlook.com>
2023-04-05 15:21:19 -0300

    testando paramentros do allure

--- test/usuarios/get.test.js
+++ test/usuarios/get.test.js
@@ -1,5 +1,5 @@
 const chai = require('chai')
-
+const allureMocha = require("allure-mocha/runtime");
 const rotaUsuarios = '/usuarios'
 const utils = require('../utils')
 
@@ -30,12 +30,13 @@ describe(rotaUsuarios + ' GET', () => {
 
   it('Query string - Nenhum usuário encontrado', async () => {
     const { body } = await request.get(rotaUsuarios).query({ _id: 'a' }).expect(200)
+    allureMocha.allure.parameter("body", body);
     chai.assert.deepEqual(body, { quantidade: 0, usuarios: [] })
   })
 
   it('Query string - Chave inexistente', async () => {
     const { body } = await request.get(rotaUsuarios).query({ inexistente: 'a' }).expect(400)
-
+    allureMocha.allure.parameter("body", body);
     chai.assert.deepEqual(body, { inexistente: 'inexistente não é permitido' })
   })
 })
